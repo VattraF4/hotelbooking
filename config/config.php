454677a2000a4d9ccaf12867d3e9e2f890b312fc
Ra Vattra <vattraramo13@gmail.com>
2025-03-27 15:04:23 +0700

    check host for database

--- config/config.php
+++ config/config.php
@@ -1,17 +1,23 @@
 <?php
 try {
-
-
-    //host
-    // define("DB_HOST", "localhost");
-    define("DB_HOST", "bh-34.webhostbox.net");
-    //database name
-    define("DB_NAME", "e4g7wad_hotel-booking");
-    //database user
-    define("DB_USER", "root");
-    //database password
-    define("DB_PASS", "");
-
+    // Check if running on localhost
+    if ($_SERVER['HTTP_HOST'] === 'localhost') {
+        // Local Development
+        define("DB_HOST", "localhost");
+        define("DB_NAME", "e4g7wad_hotel-booking"); // Your local DB name
+        define("DB_USER", "root");
+        define("DB_PASS", "");
+    } else {
+        //host
+        // define("DB_HOST", "localhost");
+        define("DB_HOST", "bh-34.webhostbox.net");
+        //database name
+        define("DB_NAME", "e4g7wad_hotel-booking");
+        //database user
+        define("DB_USER", "root");
+        //database password
+        define("DB_PASS", "");
+    }
     $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // Check connection
@@ -20,6 +26,7 @@ try {
     } else {
         echo "Connection failed: ";
     }
+
 
     //PDO: PHP Data Objects (PHP Extension) is a database access layer providing a uniform method of access to multiple databases.
 //PDO is not an abstraction layer which simply emulates the functionality of other databases;
